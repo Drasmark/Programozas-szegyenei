989d15f70d7d0c88e90483f7652aab89515e7f50
Draskóczy Márk <draskoczymark@gmail.com>
2021-03-27 13:10:14 +0100

    Aktuális

---
+++ The Teacher/Assets/SettingsMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    private void Start()
+    {
+        fullScreen.isOn = Screen.fullScreen;
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        resolutionDropdown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string resOption = resolutions[i].width + " x " + resolutions[i].height;
+            resOptions.Add(resOption);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qind)
+    {
+        QualitySettings.SetQualityLevel(qind);
+    }
+
+    public Toggle fullScreen;
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
